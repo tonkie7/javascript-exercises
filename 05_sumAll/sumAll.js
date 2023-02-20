d7032ff69020336b6a33038ec049e9ba2651a19c
Tuan D <tuan.dang@live.se>
2023-02-20 22:38:48 +0700

    sumall exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let total = 0
+    for (let i = num1; i < num2 + 1; i++) {
+        total += i
+    }
+    return total
 };
 
 // Do not edit below this line
