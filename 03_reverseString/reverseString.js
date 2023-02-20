abfdb7305d6f61ec6ea381eabbfb049ec010a8db
Tuan D <tuan.dang@live.se>
2023-02-20 14:43:17 +0700

    reversestring exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split('').reverse().join('')
+    // let reverseArray = splitString.reverse()
+    // let joinArray = reverseArray.join('')
+    return splitString
 };
 
 // Do not edit below this line
