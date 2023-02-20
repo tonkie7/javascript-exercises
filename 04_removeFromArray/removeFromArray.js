a2dbfa0b2258c49511bdf95d902e9d738768f386
Tuan D <tuan.dang@live.se>
2023-02-20 18:45:08 +0700

    removefromarray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+    // return array.filter(val => !num.includes(val))
 
+    const newArray = []
+    array.forEach(item => {
+        if(!num.includes(item)) {
+            console.log(item);
+            newArray.push(item)
+        }
+    });
+    // console.log(newArray);
+    return newArray
 };
 
 // Do not edit below this line
