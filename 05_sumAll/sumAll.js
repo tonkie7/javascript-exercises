2ee0f0d894c721539bdd9ea0977c143a3dd4c46c
Tuan D <tuan.dang@live.se>
2023-02-21 01:17:46 +0700

    sumall exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
 const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR'
+
+    if (num1 < 0 || num2 < 0) return 'ERROR'
+    
+    if (num1 > num2) {
+        const temporary = num1
+        num1 = num2
+        num2 = temporary
+    }
+
     let total = 0
-    for (let i = num1; i < num2 + 1; i++) {
+    for (let i = num1; i <= num2; i++) {
         total += i
     }
     return total
